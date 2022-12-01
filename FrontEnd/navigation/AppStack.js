ccb8d178eba3bb30c7e5befabd228fb3f254afe1
Jazmine Bullock <56108313+Jazmineox@users.noreply.github.com>
2022-11-30 23:39:11 -0500

    updated from last commit

--- FrontEnd/navigation/AppStack.js
+++ FrontEnd/navigation/AppStack.js
@@ -3,12 +3,39 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator()
 
 const AppStack = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen} />
-        </Stack.Navigator>
+        <Tab.Navigator
+            tableBarOptions={{
+                activeTintColor: '#2e64e5'
+            }}>
+            <Tab.Screen
+                name='Home'
+                component={FeedStack}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons
+                            name='home-outline'
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name='Profile'
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <IonIcons name='person-outline' color={color} size={size} />
+                    ),
+                }}
+        // <Stack.Navigator>
+        //     <Stack.Screen name='Home' component={HomeScreen} />
+        // </Stack.Navigator>
     );
 }
 
--- FrontEnd/navigation/AuthProvider.js
+++ FrontEnd/navigation/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from 'react'; 
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-community/google-signin';
+import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
 
 export const AuthContext = createContext();
 
@@ -32,6 +33,31 @@ export const AuthProvider = ({children}) => {
                         console.log({error})
                     }
                 },
+                fbLogin: async () => {
+                    try {
+                        const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+                        if (result.isCancelled) {
+                            throw 'User cancelled the login process';
+                        }
+
+                        // Once signed in, get the users AccesToken
+                        const data = await AccessToken.getCurrentAccessToken();
+
+                        if (!data) {
+                            throw 'Something went wrong obtaining access token';
+                        }
+
+                        // Create a Firebase credential with the AccessToken
+                        const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+
+                        // Sign-in the user with the credential
+                        await auth().signInWithCredential(facebookCredential);
+                    } catch (error) {
+                        console.log({error})
+                    }
+                },
+                
                 register: async (email, password) => {
                     try {
                         await auth().createUserWithEmailAndPassword(email, password);
